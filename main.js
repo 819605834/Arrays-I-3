0378cefdad1a6644e80ce6d8ca5b635b938c22ed
819605834 <124411012+819605834@users.noreply.github.com>
2026-06-11 01:37:14 +0000

    Modify fruits

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 var fruits = ["apple", "banana", "cherry", "dragon fruit", "elderberry", "fig"]; 
 
 // Write your code below
+fruits.shift();
+fruits.unshift("apricot");
+fruits[2] = "clementine";
+fruits.push("grape");
+
+console.log(fruits);
